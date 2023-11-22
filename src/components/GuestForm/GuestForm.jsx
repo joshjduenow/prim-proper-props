c51be32e5088d47ea1a0e3f4a4d70ced5820a9aa
Joshua Duenow <joshjduenow@gmail.com>
2023-11-22 08:12:15 -0600

    guest form working with no errors

---
+++ src/components/GuestForm/GuestForm.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import axios from "axios";
+
+function GuestForm({ getGuests }) {
+  let [newGuestName, setNewGuestName] = useState("");
+  let [newGuestMeal, setNewGuestMeal] = useState("false");
+
+  const addGuest = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("/guests", { name: newGuestName, kidsMeal: newGuestMeal })
+      .then((response) => {
+        // clear inputs
+        setNewGuestName("");
+        setNewGuestMeal(false);
+
+        getGuests();
+      })
+      .catch((err) => {
+        alert("Error Adding Guest");
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <h2>Add a new guest</h2>
+      <form onSubmit={addGuest}>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newGuestName}
+          onChange={(evt) => setNewGuestName(evt.target.value)}
+        />
+        <div>
+          Would this guest like a kid's meal?
+          <div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value={true}
+                  checked={newGuestMeal === "true"}
+                  name="kidsMeal"
+                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
+                />
+                Yes, this guest would like a Kid's Meal
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value={false}
+                  checked={newGuestMeal === "false"}
+                  name="kidsMeal"
+                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
+                />
+                No, this guest would not like a Kid's Meal
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit">Add Guest</button>
+      </form>
+    </>
+  );
+}
+export default GuestForm;
